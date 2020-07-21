72473ea44abebb3cf165468f3a990022390d0d5f
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-07-21 12:39:55 -0700

    added connection to database and access to pull data

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express()
 
@@ -13,11 +14,21 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
+app.use(cors())
 
+//get articles 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
-app.use(cors())
+
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
+     .then(articles => {
+       res.json(articles)
+     }).catch(next)
+})
+
 
 app.use(function errorHandler(error, req, res, next) {
     let response
